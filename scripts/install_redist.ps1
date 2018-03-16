b0de6fdb0a3e9a0556a6749e8ebf563da8440899
Andreas Axelsson <axl@directivegames.com>
2018-03-16 21:30:47 +0800

    Fix typo in log redirection

--- scripts/install_redist.ps1
+++ scripts/install_redist.ps1
@@ -113,9 +113,9 @@ $stdOutLog = "$env:TEMP\stdout.log"
 $stdErrLog = "$env:TEMP\stderr.log"
 $pipLog = ($setuplogfolder + "\" + "pip.log")
 Start-Process "c:\python27\python.exe" -Wait -ArgumentList ("-m pip install --upgrade pip") -RedirectStandardOutput $stdOutLog -RedirectStandardError $stdErrLog
-Get-Content $stdOutLog $stdErrLog | Out-File $pipLog -Append
+Get-Content $stdOutLog, $stdErrLog | Out-File $pipLog -Append
 Start-Process "c:\python27\scripts\pip.exe" -Wait -ArgumentList ("install -r c:\drift-serverdaemon\requirements.txt") -RedirectStandardOutput $stdOutLog -RedirectStandardError $stdErrLog
-Get-Content $stdOutLog $stdErrLog | Out-File $pipLog -Append
+Get-Content $stdOutLog, $stdErrLog | Out-File $pipLog -Append
 
 $newTag = New-Object Amazon.EC2.Model.Tag
 $newTag.Key = "drift-status"
